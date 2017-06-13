6bcb71cca70a485772afedeb7199b191dfba207c
Jojo\JOJO <johannes.wirth94@web.de>
2017-06-13 11:22:33 +0200

    no message

---
+++ src/net/ssjp/view/ProcessLogUploadView.java
@@ -0,0 +1,43 @@
+package net.ssjp.view;
+
+import java.io.IOException;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.model.UploadedFile;
+
+import net.ssjp.controller.ProcessLogUploadController;
+
+public class ProcessLogUploadView extends FileUploadView{
+
+	public ProcessLogUploadView(UploadedFile file){
+		super(file);
+	}
+	
+	
+	@Override
+	public void upload() {
+		// TODO Auto-generated method stub
+        if(getFile() != null) {
+        	System.out.println(42342);
+        	FacesMessage message;
+        	ProcessLogUploadController controller = new ProcessLogUploadController();
+            try {
+				controller.saveData(getFile());
+				message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Success", getFile().getFileName() + " is uploaded.");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Failure", "Upload of" + getFile().getFileName() + " failed or no file.");
+				e.printStackTrace();
+			}
+            
+            FacesContext.getCurrentInstance().addMessage(null, message);
+            
+
+        }
+	}
+	
+	
+	
+}
